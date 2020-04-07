52ffc71b1a1945f29a768641e4f494516891a17b
ronaldm <rontype1@gmail.com>
2020-04-07 14:03:50 -0700

    fix build tag error

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
 set -e
-
 use_tag="rontype1/uwsgi-nginx:$NAME"
-use_dated_tag="${use_tag}-$(date -I)"
-
 docker build -t "$use_tag" "$BUILD_PATH"
-
-docker tag "$use_tag"
-
 docker push "$use_tag"
